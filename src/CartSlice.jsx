9c7d2ce3142ec37bb1998f8e64679b0bd0a949f3
fabricemogini <fmogini@gmail.com>
2026-02-02 16:17:16 +0100

    Remove unused imports in CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -100,6 +99,9 @@ const CartItem = ({ onContinueShopping }) => {
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 export default CartSlice.reducer;
+
+
+
 
 
 
